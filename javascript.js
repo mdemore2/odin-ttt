a3177abc3987a7531807f7455e3479c758527ea4
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-10-28 15:09:58 -0400

    Completed functionality
    Added game state and tracking to javascript.js
    Added dialog for win modal in index.html

---
+++ javascript.js
@@ -0,0 +1,61 @@
+console.log('Loaded')
+let board = [["", "", ""],["", "", ""],["", "", ""]];
+let turn = 'x';
+const dialog = document.querySelector('dialog');
+
+
+let spaces = document.querySelectorAll('.space')
+spaces.forEach((space) => {
+    console.log(`listener added for ${space.id}`)
+    space.addEventListener('click', updateBoard)
+})
+
+function updateBoard(e) {
+    console.log('DIV CLICKED')
+    e.currentTarget.innerHTML = turn;
+    let spaceID = e.currentTarget.id;
+    let row = Math.floor(spaceID / 3);
+    let col = spaceID % 3;
+    board[row][col] = turn;
+    turn = turn == 'x' ? 'o' : 'x';
+    e.currentTarget.removeEventListener('click',updateBoard)
+    let winner = checkWin();
+    if (winner) {
+        dialog.innerHTML = `${winner} wins! Game over!`
+        dialog.showModal();
+    }
+}
+
+function checkWin(){
+    for (let row = 0; row <  3; row++){
+        if (board[row].every(val => val === board[row][0])) {
+            return board[row][0];
+        }
+    }
+    for (let col = 0; col < 3; col++){
+        let colArr = [];
+        for (let row = 0; row <  3; row++){
+            colArr.push(board[row][col])
+        }
+        if (colArr.every(val => val === colArr[0])) {
+            return colArr[0];
+        }
+    }
+    let diag1 = [
+        board[0][0],
+        board[1][1],
+        board[2][2]
+    ]
+    let diag2 = [
+        board[2][0],
+        board[1][1],
+        board[0][2]
+    ]
+    if (diag1.every(val => val === diag1[0])) {
+        return diag1[0];
+    }
+    if (diag2.every(val => val === diag2[0])) {
+        return diag2[0];
+    }
+    return false;
+}
